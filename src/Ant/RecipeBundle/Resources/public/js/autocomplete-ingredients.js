5b3320b55580d6dfc18ce767e6b6ce53844e4d5a
antonio <antonio@debian-ant3>
2016-11-17 17:37:54 +0100

    17novstyles

--- src/Ant/RecipeBundle/Resources/public/js/autocomplete-ingredients.js
+++ src/Ant/RecipeBundle/Resources/public/js/autocomplete-ingredients.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $( "#myTextField" ).focus();
     elementosDerecha = [];
      $("#myTextField").on('keyup', function() {                           
              var input = $(this).val(); 
